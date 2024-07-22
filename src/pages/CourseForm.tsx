b9f9a6f7509638f51b36d3bc6be2266d3ec6f585
Polina Limonova <polina98_08@mail.ru>
2024-07-22 15:55:57 +0300

    Course form created

--- src/pages/CourseForm.tsx
+++ src/pages/CourseForm.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import Button from "../components/Button/Button";
 
 const CourseForm = () => {
-  return <div>CourseForm</div>;
+  return (
+    <div className="form_container">
+      <div>
+        <div className="title">Оставить заявку</div>
+        <form className="fields">
+          <input type="text" placeholder="Ваше имя" />
+          <input type="text" placeholder="Телефон" />
+          <input type="text" placeholder="Эл. почта" />
+          <Button
+            text="Отправить"
+            style={{ width: "max-content", marginTop: "20px" }}
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default CourseForm;
