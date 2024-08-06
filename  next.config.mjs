b533ed3b58e0f6819eddece654c8497c0e2e9c51
Polina Limonova <polina98_08@mail.ru>
2024-08-06 15:53:49 +0300

    Install plugin

---  next.config.mjs
+++  next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 const nextConfig = {
   output: "export", // Outputs a Single-Page Application (SPA).
   distDir: "./dist", // Changes the build output directory to `./dist/`.
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
