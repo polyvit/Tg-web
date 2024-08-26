98ede72744d7565becde42d8ed7d30170514153f
Polina Limonova <polina98_08@mail.ru>
2024-08-26 15:40:30 +0300

    Add mongoose

---
+++ src/lib/mongoose.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const uri = process.env.MONGO_DATABASE_URL;
+
+if (!uri) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env"
+  );
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose.connect(uri, opts).then((mongoose) => {
+      console.log("Db connected!!");
+      return mongoose;
+    });
+  }
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default dbConnect;
